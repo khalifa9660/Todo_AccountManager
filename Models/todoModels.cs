52aef478f9928dca9b2eef1e636e8fc4372e6e00
khalifa Tambadou <khalifa.tambadou@perrenot.eu>
2023-11-16 14:04:05 +0100

    Modification

--- Models/todoModels.cs
+++ Models/todoModels.cs
@@ -1,10 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+
 namespace Todo_AccountManager.Models
 {
-    public class todoModels : Controller
+    public class todoModels
     {
       public int Id { get; set; }
-      public string todoList { get; set; }
+      public List<Item> TodoList { get; set; }
+        public string Label { get; set; }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Libelle { get; set; }
+        public int Todo { get; set; }
+        public string Statut { get; set; }
     }
 }
